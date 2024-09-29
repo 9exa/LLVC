b123c74df72d8a35f92e8714ee79a1cefce4177e
9exa <resosudarmo.wisesa@gmail.com>
2024-09-30 03:45:25 +1000

    added ability to only download the hubert_base checkpoint (which is necessary for training)

--- download_models.py
+++ download_models.py
@@ -1,6 +1,22 @@
 # download models from KoeAI/llvc on Huggingface Hub
 from huggingface_hub import snapshot_download
+import argparse
 
-# download models from KoeAI/llvc on Huggingface Hub
-snapshot_download(repo_id="KoeAI/llvc",
-                  local_dir='llvc_models')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hubert_only', action='store_true', 
+                        help='whether to only download the Hubert model')
+    args = parser.parse_args()
+
+    # download models from KoeAI/llvc on Huggingface Hub
+    if args.hubert_only:
+        snapshot_download(repo_id="KoeAI/llvc",
+                        local_dir='llvc_models',
+                        allow_patterns=["*hubert_base*"])
+    else:
+        snapshot_download(repo_id="KoeAI/llvc",
+                        local_dir='llvc_models')
+    
+
+if __name__ == "__main__":
+    main()
